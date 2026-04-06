9defea5c427069eb9cb054a8d39cc89377f0cd70
pranalithakkar <97580049+pranalithakkar@users.noreply.github.com>
2026-04-06 19:40:07 -0400

    Update quantization

--- serving/quantize.py
+++ serving/quantize.py
@@ -1,8 +1,8 @@
-from optimum.onnxruntime import ORTModelForSequenceClassification
-from optimum.onnxruntime.configuration import AutoQuantizationConfig
-from optimum.onnxruntime import ORTQuantizer
-
-qconfig = AutoQuantizationConfig.avx512_vnni(is_static=False, per_channel=False)
-quantizer = ORTQuantizer.from_pretrained('./onnx_model')
-quantizer.quantize(save_dir='./onnx_model_quantized', quantization_config=qconfig)
-print('Quantized model saved to ./onnx_model_quantized')
+from onnxruntime.quantization import quantize_dynamic, QuantType
+import os
+input_path = 'onnx_model/model.onnx'
+output_path = 'onnx_model_quantized/model.onnx'
+os.makedirs('onnx_model_quantized', exist_ok=True)
+print(f'Quantizing {input_path} -> {output_path}')
+quantize_dynamic(input_path, output_path, weight_type=QuantType.QInt8)
+print('Quantization complete.')
